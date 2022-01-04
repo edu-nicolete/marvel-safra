5ea9e0c37361af76bb1515b62cdf1749370b6ce2
Eduardo Nicolete <edunicol@gmail.com[>
2022-01-04 14:52:26 -0300

    correcao package

---
+++ serve.js
@@ -0,0 +1,14 @@
+'use strict'
+var express = require('express');
+var cors = require('cors');
+
+// App
+var app = express();
+app.use(cors());
+app.set('port', (process.env.PORT || 8000));
+
+app.use('/', express.static(__dirname + '/app'));
+
+app.listen(app.get('port'), function() {
+  console.log("running: port", app.get('port'));
+});
